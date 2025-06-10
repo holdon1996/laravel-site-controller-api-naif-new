9a456e94f4ca2b39b92946fe5a701215fcffe5cb
thinhhung <hungt@thefirstone.jp>
2025-06-10 17:35:06 +0900

    feat: HKTV-665 api get option TL

--- src/routes/sc.php
+++ src/routes/sc.php
@@ -14,5 +14,6 @@ Route::prefix('/api/sc')->middleware('api.sc.auth')->group(function () {
         Route::post('/bulk-empty-room', [TlLincolnController::class, 'getBulkEmptyRoom'])->name('tllincoln-api.get-bulk-empty-room');
         Route::post('/price-plan', [TlLincolnController::class, 'getPricePlan'])->name('tllincoln-api.get-price-plan');
         Route::post('/bulk-price-plan', [TlLincolnController::class, 'getBulkPricePlan'])->name('tllincoln-api.get-bulk-price-plan');
+        Route::post('/option', [TlLincolnController::class, 'getOption'])->name('tllincoln-api.get-option');
     });
 });
